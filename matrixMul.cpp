ceb6d1060f64b528943defcb7ba84065ee48d50a
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2024-09-15 12:00:48 -0400

    Create matrixMul.cpp

---
+++ matrixMul.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void printMatrixMul(int** mat1, int r1, int c1, int** mat2, int r2, int c2) {
+    // TODO: create a 2D dynamic array for the resulting matrix
+
+    // TODO: compute and store the resulting element values in the resulting matrix
+
+    // TODO: display the element values of the resulting matrix using cout
+    cout << endl << "My resulting matrix is: " << endl;
+
+    // TODO: deallocate the dynamic memory from the 2D array
+}
